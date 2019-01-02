732965d8c6e2715efccc854177ed93bd231e4ba2
Prashant <Prashant@DESKTOP-FBDO90J>
2019-01-02 13:43:44 +0530

    02newch

--- MarathaBusinessApi/MarathaBusinessApi/Controllers/DashboardController.cs
+++ MarathaBusinessApi/MarathaBusinessApi/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MarathaBusinessApi.Entity;
+using MarathaBusinessApi.Models;
 
 namespace MarathaBusinessApi.Controllers
 {
@@ -27,11 +28,47 @@ namespace MarathaBusinessApi.Controllers
 
         #endregion
 
-        #region Delete Business Man Registrations
+        #region Edit And Delete Business Man Registrations
 
-        public ActionResult DeleteBusinessMan()
+       public ActionResult Edit(int id)
         {
-            return View();
+            var result = _db.tblBusinessManRegistrations.Find(id);
+            return View(result);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(tblBusinessManRegistration model)
+        {
+            tblBusinessManRegistration _objBusiness = (from psd in _db.tblBusinessManRegistrations where( psd.Bid==model.Bid) select psd).FirstOrDefault();
+
+            _objBusiness.Name = model.Name;
+            _objBusiness.MobileNo = model.MobileNo;
+            _objBusiness.Occupation = model.Occupation;
+            _objBusiness.PhoneNo = model.PhoneNo;
+            _objBusiness.Status = model.Status;
+            _objBusiness.StoreName = model.StoreName;
+            _objBusiness.Email = model.Email;
+            _objBusiness.Document = model.Document;
+            _objBusiness.City = model.City;
+            _objBusiness.Address = model.Address;
+
+            _db.Entry(_objBusiness).State = System.Data.Entity.EntityState.Modified;
+            _db.SaveChanges();
+
+
+            return RedirectToAction("DisplayAllBusinessMan", "Dashboard");
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var result = _db.tblBusinessManRegistrations.Find(id);
+            _db.Entry(result).State = System.Data.Entity.EntityState.Deleted;
+            _db.SaveChanges();
+
+            return RedirectToAction("DisplayAllBusinessMan", "Dashboard");
         }
 
         #endregion
